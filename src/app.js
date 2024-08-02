27dec9ba730f3e1534ef7d25e21a2105f7acccb3
Harsh Bailurkar <harshbailurkar@gmail.com>
2024-08-02 21:26:03 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,8 +9,6 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Authorization",
   })
 );
 
